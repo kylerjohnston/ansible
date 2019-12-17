769336017e788518efc4b667a242d37ba9409b9c
Kyle Johnston <kylerjohnston@gmail.com>
2019-12-17 18:00:26 -0500

    Installs alertmanager #15
    Also makes it so check-drive.sh reports `1` if the drive is mounted and
    `0` if it's not, because that makes way more sense.

--- roles/descartes/files/check-drive.sh
+++ roles/descartes/files/check-drive.sh
@@ -4,9 +4,9 @@ MOUNTPOINT=/var/alexandria
 
 mount | grep $MOUNTPOINT > /dev/null 2>&1
 if [ $? != 0 ]; then
-    STATUS=1
-else
     STATUS=0
+else
+    STATUS=1
 fi
 
 echo "descartes_alexandria_mounted ${STATUS}" > /var/www/html/metrics
