d78a6d770688a18b12cb54a0e46956efdafecf71
Kyle Johnston <kylerjohnston@gmail.com>
2019-12-31 15:44:41 -0500

    Script to generate environment variable file

---
+++ generate-env.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+env_vars = {
+  'SES_SMTP_USER' => {'field' => 'username', 'name' => 'SES SMTP muumuus'},
+  'SES_SMTP_PASSWORD' => {'field' => 'password', 'name' => 'SES SMTP muumuus'}
+}
+
+email = 'kylerjohnston@gmail.com'
+env_file = 'env.sh'
+
+if `lpass status`.include?('Not')
+  system('lpass', 'login', email)
+end
+
+f = File.open(env_file, 'w')
+f.puts "#!/bin/bash"
+
+env_vars.each_key do |var|
+  value = `lpass show --#{env_vars[var]['field']} "#{env_vars[var]['name']}"`
+  f.puts "export #{var}=#{value}"
+end
+
+File.chmod(0770, env_file)
+f.close
