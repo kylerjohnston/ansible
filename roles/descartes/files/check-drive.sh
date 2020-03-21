049a830317274e7c98fa907d9a4b13f24e6ee170
Kyle Johnston <kylerjohnston@gmail.com>
2020-03-21 18:41:30 -0400

    These status values were backwards? How was it ever working??

--- roles/descartes/files/check-drive.sh
+++ roles/descartes/files/check-drive.sh
@@ -4,9 +4,9 @@ MOUNTPOINT=/var/alexandria
 
 if mount | grep $MOUNTPOINT > /dev/null 2>&1
 then
-    STATUS=0
-else
     STATUS=1
+else
+    STATUS=0
 fi
 
 sed -Ei "s/descartes_alexandria_mounted [0-9]+/descartes_alexandria_mounted ${STATUS}/g" /var/www/html/metrics
