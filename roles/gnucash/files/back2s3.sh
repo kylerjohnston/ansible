398ccc1c7c419d1c2e0a07e767a93866386be8a8
Kyle Johnston <kylerjohnston@gmail.com>
2021-03-27 12:46:35 -0400

    delete back2s3 stuff

--- roles/gnucash/files/back2s3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Backing up to S3"
-/usr/bin/aws --profile descartes-s3-uploader s3 cp /home/krj/Documents/financial/register-xml/register.gnucash s3://spinoza-web/gnucash-registers/
-
-exit $?
