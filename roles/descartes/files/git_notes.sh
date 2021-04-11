c83e45edb84ee392dd4e57e63b9b617a9405b06b
Kyle Johnston <kylerjohnston@gmail.com>
2021-04-11 09:09:56 -0400

    git_notes.sh

--- roles/descartes/files/git_notes.sh
+++ roles/descartes/files/git_notes.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo $(date)
+
 cd /home/krj/home-docs/notes
-if ! $(git status | grep 'nothing to commit'); then
+
+gstatus=$(git status --porcelain)
+
+if [ ${#gstatus} -ne 0 ]; then
     git add -A
     git commit -m "$(date)"
+    git pull --rebase
     git push origin main
+  else
+    git pull --rebase
 fi
+
+echo "Success"
